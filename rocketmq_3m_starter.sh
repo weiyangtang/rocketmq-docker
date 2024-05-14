a8d8c285f5ef29968ae89181854768f833f0642a
tangweiyang <weiyang_tang@126.com>
2024-05-14 22:19:48 +0800

    only one shell

--- rocketmq_3m_starter.sh
+++ rocketmq_3m_starter.sh
@@ -7,4 +7,17 @@ for port in $(seq 10911 2000 14911); do \
   PORT=${port} BROKER_ROLE="SYNC_MASTER" BROKER_ID=0 IP_ADDR=${ip}  envsubst < broker_conf.tmpl > broker-${port}-master/conf/broker.conf;
   echo "broker-${port}-master dir and config ok!";
 done
+ mkdir -p namesrv/logs;
+echo "mkdir namesrv/logs";
+
+ NAMESRV_IP=${ip}  envsubst < docker-compose.yml.tmpl > docker-compose.yml;
+ echo "generate docker-compose.yml successful";
 chown -R 3000:3000 *;
+
+echo "start docker compose up -d ";
+
+docker compose up -d;
+
+echo "docker ps, check rocketmq cluster status";
+
+echo "brower url: http://localhost:8080/#/cluster";
